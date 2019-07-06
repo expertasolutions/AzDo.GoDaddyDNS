78658da62d1b4bf4ec3262a44bcb3267f0f46be8
lturmel <lturmel@expertasolutions.com>
2019-07-06 09:46:52 -0400

    Initial project setup
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# GoDaddyDNS
+## Objective
+
+Tasks packages to manage GoDaddy DNS Zone from Azure DevOps release pipeline.
+
+Tasks provides:
+- A Record Manager
+- CName Record Manager
+
+This task package is compatible with:
+- Hosted macOS build agent (supported)
+- Hosted VS2017 (supported)
+- Any private build agent with Powershell and Azure CLI installed
+
+## A Record Manager (required parameters)
+![ARecord_Task_inputs](img/ARecord_v4.jpg)
+
+## CNAME Record Manager (required parameters)
+![CNAMERecord_Task_inputs](img/cnameRecord_v4.jpg)
+
+# Requirements
+
+- Azure CLI must be installed on the build agent
---
+++ aRecordManager/src/manageadnszone.js
@@ -0,0 +1,48 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+
+var tl = require('azure-pipelines-task-lib');
+var shell = require('node-powershell');
+
+try {
+    
+    var azureEndpointSubscription = tl.getInput("azureSubscriptionEndpoint", true);
+    var resourceGroupName = tl.getInput("resourceGroupName", true);
+    var domainName = tl.getInput("domainName", true);
+    var aName = tl.getInput("aName", true);
+
+    var actionType = tl.getInput("actionType", true);
+
+    var ipRequired = actionType == "createUpdate";
+
+    var ipAddress = tl.getInput("ipAddress", ipRequired);
+    var ttl = tl.getInput("ttl", true);
+    
+    var subcriptionId = tl.getEndpointDataParameter(azureEndpointSubscription, "subscriptionId", false);
+
+    var servicePrincipalId = tl.getEndpointAuthorizationParameter(azureEndpointSubscription, "serviceprincipalid", false);
+    var servicePrincipalKey = tl.getEndpointAuthorizationParameter(azureEndpointSubscription, "serviceprincipalkey", false);
+    var tenantId = tl.getEndpointAuthorizationParameter(azureEndpointSubscription,"tenantid", false);
+
+    console.log("SubscriptionId: " + subcriptionId);
+    console.log("ServicePrincipalId: " + servicePrincipalId);
+    console.log("ServicePrincipalKey: " + servicePrincipalKey);
+    console.log("TenantId: " + tenantId);
+    console.log("ResourceGroupName: " + resourceGroupName);
+    console.log("ActionType: " + actionType);
+    console.log("DomainName: " + domainName);
+    console.log("A Name: " + aName);
+    console.log("Ip Address: " + ipAddress);
+    console.log("TTL (seconds): " + ttl);
+    
+} catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
+}
