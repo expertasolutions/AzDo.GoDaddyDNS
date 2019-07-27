c56dbdb231211597fd1864748a0037abb71b797c
lturmel <lturmel@expertasolutions.com>
2019-07-27 10:50:28 -0400

    A DNS record task implementation
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- aRecordManager/src/manageadnszone.js
+++ aRecordManager/src/manageadnszone.js
@@ -13,30 +13,111 @@ var tl = require('azure-pipelines-task-lib');
 var shell = require('node-powershell');
 
 try {
-
     var goDaddyEndpoint = tl.getInput("godaddyAccount", true);
+    var url = tl.getEndpointUrl(goDaddyEndpoint, true);
+    var goDaddyApiUrl = url.replace("http://", "").replace("https://", "").replace('/', '');
     var goDaddyToken = tl.getEndpointAuthorizationParameter(goDaddyEndpoint, "apitoken", false);
     var goDaddySecret = tl.getEndpointAuthorizationParameter(goDaddyEndpoint, "apisecret", false);
 
-    var goDaddyToken = tl.getInput("godaddyKey", true);
-    var goDaddySecret = tl.getInput("godaddySecret", true);
     var domainName = tl.getInput("domainName", true);
     var aName = tl.getInput("aName", true);
-
+    var ipAddress = tl.getInput("ipAddress", true);
     var actionType = tl.getInput("actionType", true);
+    var ttl = +(tl.getInput("ttl", true));
 
-    var ipRequired = actionType == "createUpdate";
+    console.log("GoDaddy API URL: " + goDaddyApiUrl);
+    console.log("GoDaddy API Token: " + goDaddyToken);
+    console.log("GoDaddy API Secret: " + goDaddySecret);
 
-    var ipAddress = tl.getInput("ipAddress", ipRequired);
-    var ttl = tl.getInput("ttl", true);
-    
-    console.log("GoDaddyKey: " + goDaddyToken);
-    console.log("GoDaddySecret: " + goDaddySecret);
     console.log("ActionType: " + actionType);
     console.log("DomainName: " + domainName);
-    console.log("A Name: " + aName);
-    console.log("Ip Address: " + ipAddress);
-    console.log("TTL (seconds): " + ttl);
+    console.log("A: " + aName);
+    console.log("IPAddress: " + ipAddress);
+    console.log("TTL: " + ttl);
+
+    var authToken = "sso-key " + goDaddyToken + ":" + goDaddySecret;
+
+    if(actionType === "createUpdate") {
+        const data = JSON.stringify([{
+            "data": ipAddress,
+            "name": aName,
+            "type": "A",
+            "ttl": ttl
+        }]);
+
+        var options = {
+            host: goDaddyApiUrl,
+            path: '/v1/domains/' + domainName + '/records/A/' + aName,
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+                "Content-Length": data.length,
+                "Authorization": authToken
+            }
+        };
+    
+        const req = http.request(options, response => { });
+    
+        req.on('error', error => {
+            tl.setResult(tl.TaskResult.Failed, error || 'run() failed');
+        });
+    
+        req.write(data);
+        req.end();
+
+    } else if(actionType === "remove") {
+        // List the current A Records
+        var listOptions = {
+            host: goDaddyApiUrl,
+            path: '/v1/domains/' + domainName + '/records/A',
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": authToken
+            }
+        };
+        http.request(listOptions, r => {
+            var body = '';
+
+            r.on('data', d => {
+                body += d;
+            });
+              
+            r.on('end', () => {
+                var aList = JSON.parse(body);
+                const index = aList.findIndex(x=> x.name == cname);
+                if(index > -1){
+                   aList.splice(index, 1);
+                }
+
+                // Update All A records without old one
+                const data = JSON.stringify(aList);
+
+                var options = {
+                    host: goDaddyApiUrl,
+                    path: '/v1/domains/' + domainName + '/records/A/',
+                    method: 'PUT',
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Content-Length": data.length,
+                        "Authorization": authToken
+                    }
+                };
+
+                const req = http.request(options, response => { });
+            
+                req.on('error', error => {
+                    tl.setResult(tl.TaskResult.Failed, error || 'run() failed');
+                });
+            
+                req.write(data);
+                req.end();
+            });
+            
+        }).on("error", err => {
+            tl.setResult(tl.TaskResult.Failed, err || 'run() failed');
+        }).end();
+    }
     
 } catch (err) {
     tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
